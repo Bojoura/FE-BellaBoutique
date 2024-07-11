df4fd13ac06ac9cecc0b093365765548c51ae35b
Bojoura <bo.efendi@gmail.com>
2024-07-11 19:04:03 +0200

    Setup productpage

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,11 +1,42 @@
-import './Product.css';
+import { useState, useEffect } from 'react';
+import "./Product.css";
+import axios from 'axios';
 
-const Product = () => {
+
+const ProductList = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const response = await axios.get('https://fakestoreapi.com/products');
+
+                setProducts(response.data);
+            } catch (error) {
+                console.error('Error fetching products:', error);
+            }
+        };
+
+        fetchProducts();
+    }, []);
+
     return (
-        <div>
-            <h1>Product</h1>
-        </div>
+        <>
+            <div className="product-container container">
+                <h1>Kleding</h1>
+
+                <div className="product-list">
+                    {products.map((product) => (
+                        <div key={product.id} className="product-item">
+                            <img src={product.image} alt={product.title}/>
+                            <h3>{product.title}</h3>
+                            <p>Prijs: € {product.price}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
     );
 };
 
-export default Product;
+export default ProductList;
