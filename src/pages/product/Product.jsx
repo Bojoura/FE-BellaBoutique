65c87066560db5f1a4928c30b6a3a977d92ec641
Bojoura <151749193+Bojoura@users.noreply.github.com>
2024-11-04 22:02:35 +0100

    Merge pull request #4
    * Setup productpage
    
    * Work in progress: Adding a productCard
    
    * Work in progress: Adding a hook for products
    
    * Adjusting the productpage

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,11 +1,39 @@
-import './Product.css';
+import "./Product.css";
+import ProductCard from "../../components/productcard/ProductCard.jsx";
+import useProducts from "../../components/hooks/useProducts.jsx";
+import {useSearchParams} from "react-router-dom";
 
-const Product = () => {
+const ProductList = () => {
+    const {products, loading, error} = useProducts();
+    const [searchParams] = useSearchParams();
+
+    if (loading) {
+        return <p>Laden...</p>;
+    }
+
+    if (error) {
+        return <p>{error}</p>;
+    }
+
+    const query = searchParams.get("search");
+    let filteredProducts = products;
+
+    if (query) {
+        filteredProducts = products.filter((product) => product.title.toLowerCase().includes(query.toLowerCase()));
+        if  (filteredProducts.length === 0) {
+            return <p>Geen producten gevonden</p>;
+        }
+    }
+
     return (
-        <div>
-            <h1>Product</h1>
+        <div className="product-container">
+            <div className="product-list">
+                {filteredProducts.map((product) => (
+                    <ProductCard key={product.id} product={product} />
+                ))}
+            </div>
         </div>
     );
 };
 
-export default Product;
+export default ProductList;
