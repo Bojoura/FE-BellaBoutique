962cf297dab9922ad95a0a745c46e8f2fc953c51
Bojoura <bo.efendi@gmail.com>
2024-11-04 19:39:11 +0100

    Work in progress: Adding a hook for products

---
+++ src/components/hooks/UseProducts.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useProducts = () => {
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                setLoading(true);
+                setError(null);
+
+                const response = await axios.get('http://localhost:8080/api/products');
+                setProducts(response.data);
+            } catch (error) {
+                setError('Er is een fout opgetreden bij het ophalen van de producten');
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchProducts();
+    }, []);
+
+    return { products, loading, error };
+};
+
+export default useProducts;
