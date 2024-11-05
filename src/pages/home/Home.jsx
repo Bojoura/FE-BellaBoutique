a695769b8385f99cbfe36da25f10f68fda6ea259
Bojoura <bo.efendi@gmail.com>
2024-11-05 19:13:58 +0100

    Making the homepage

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,39 @@
 import './Home.css';
+import womenShop from "../../assets/women-shop.jpg";
+import useProducts from '../../components/hooks/useProducts';
+import { Link } from 'react-router-dom';
+import ProductCard from "../../components/productcard/ProductCard.jsx";
 
 const Home = () => {
+    const { products, loading, error } = useProducts();
+    if (loading) {
+        return <p>Laden...</p>;
+    }
+
+    if (error) {
+        return <p>{error}</p>;
+    }
+
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="homepage">
+            <section className="hero">
+                <div className="hero-text">
+                    <h1>{"Welkom bij Bella's Boutique"}</h1>
+                    <p>Ontdek de nieuwste mode en trends</p>
+                    <Link to="/product">
+                        <button className="shop-button">Shop Nu</button>
+                    </Link>
+                </div>
+                <div className="background"><img src={womenShop}/></div>
+            </section>
+            <section className="featured-products">
+                <h2>Uitgelichte Producten</h2>
+                <div className="products">
+                    {products.slice(0, 3).map((product) => (
+                        <ProductCard key={product.id} product={product} />
+                    ))}
+                </div>
+            </section>
         </div>
     );
 };
