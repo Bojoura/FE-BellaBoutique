19986349b27bc6ceb8eb279f0ff98450c3f7236d
Bojoura <bo.efendi@gmail.com>
2024-11-05 19:53:33 +0100

    Adding authenticationcontext

---
+++ src/context/AuthenticationContext.jsx
@@ -0,0 +1,85 @@
+import {createContext, useContext, useEffect, useState} from 'react';
+import { useNavigate } from 'react-router-dom';
+import { jwtDecode } from "jwt-decode";
+import axios from "axios";
+import checkTokenValidation from "../helpers/CheckTokenValidation.jsx";
+
+const AuthContext = createContext(
+    // isAuth: false,
+    // user: null,
+    // login: () => {},
+    // logout: () => {},
+);
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthContextProvider = ({ children }) =>{
+    const [auth, setAuth] = useState({
+        isAuth: false,
+        user: '',
+        status: 'pending',
+    });
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const storedToken = localStorage.getItem('jwt');
+        if (storedToken && checkTokenValidation(storedToken)) {
+            void login(storedToken);
+        } else {
+            void logout();
+        }
+    }, []);
+
+    const login = async (jwt) => {
+        const decodedToken = (jwtDecode(jwt));
+        console.log(decodedToken);
+        localStorage.setItem('jwt', jwt);
+        try {
+            const response = await axios.get(`http://localhost:8080/users/?username=${decodedToken.sub}`, {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${jwt}`,
+                },
+            });
+
+            setAuth({
+                ...auth,
+                isAuth: true,
+                user: {
+                    username: response.data.username,
+                    email: response.data.email,
+                    authority: response.data.authority,
+                },
+                status: 'done',
+            });
+        } catch (error) {
+            alert(error);
+        }
+    }
+
+    function logout(redirect = true) {
+        setAuth({
+            ...auth,
+            isAuth: false,
+            user: '',
+            status: 'done',
+        });
+        localStorage.removeItem('jwt');
+        localStorage.removeItem('cart');
+        if (redirect) {
+            navigate('/');
+        }
+    }
+
+    const contextData = {
+        isAuth: auth.isAuth,
+        user: auth.user,
+        login,
+        logout,
+    };
+    return (
+        <AuthContext.Provider value={contextData}>
+            {auth.status === 'done' ? children : <p>Loading...</p>}
+        </AuthContext.Provider>
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import App from './App.jsx'
 import './index.css'
 import {BrowserRouter as Router} from 'react-router-dom'
 import {ShopCartProvider} from "./context/ShopCartContext.jsx";
+import {AuthContextProvider} from "./context/AuthenticationContext.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <Router>
-            <ShopCartProvider>
-                <App />
-            </ShopCartProvider>
+            <AuthContextProvider>
+                <ShopCartProvider>
+                    <App />
+                </ShopCartProvider>
+            </AuthContextProvider>
         </Router>
     </React.StrictMode>,
 )
