e0e952f1b189bdf2aa352cc694c61f5051bf40fd
Bojoura <bo.efendi@gmail.com>
2025-01-02 13:45:06 +0100

    Added routes into the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
-import {Routes, Route} from 'react-router-dom'
 import './App.css'
 import Navbar from "./components/navbar/Navbar";
-import Home from "./pages/home/Home";
-import Account from "./pages/account/Account";
-import Product from "./pages/product/Product";
 import Footer from "./components/footer/Footer";
 import Bar from "./components/bar/Bar";
-import ProductDetail from "./pages/product/ProductDetail";
-import ShopCart from "./pages/shopcart/ShopCart";
-import Contact from "./pages/contact/Contact";
-import AccountDetail from "./pages/account/AccountDetail";
+import AppRoutes from "./Routes.jsx";
 
 function App() {
   return (
@@ -20,15 +13,7 @@ function App() {
         </header>
 
         <main>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/product" element={<Product/>}/>
-                <Route path="/product/:id" element={<ProductDetail/>} />
-                <Route path="/cart" element={<ShopCart/>}/>
-                <Route path="/account" element={<Account/>}/>
-                <Route path="/accountdetail" element={<AccountDetail/>}/>
-                <Route path="/contact" element={<Contact/>}/>
-            </Routes>
+           <AppRoutes />
         </main>
 
         <Footer
