a0fb0fdecb53ead24150e261a9d3200a6c11004d
Bojoura <bo.efendi@gmail.com>
2025-01-02 13:45:53 +0100

    Added the usehandleselectproduct

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,11 +1,17 @@
 import "./Product.css";
 import ProductCard from "../../components/productcard/ProductCard";
 import useProducts from "../../components/hooks/useProducts";
-import {useSearchParams} from "react-router-dom";
+import {useNavigate, useSearchParams} from "react-router-dom";
 
 const ProductList = () => {
     const {products, loading, error} = useProducts();
     const [searchParams] = useSearchParams();
+    const navigate = useNavigate();
+
+    const useHandleSelectProduct = (id) => {
+        console.log(`Product ${id} is geselecteerd`);
+        navigate(`/product/${id}`)
+    }
 
     if (loading) {
         return <p>Laden...</p>;
@@ -20,16 +26,18 @@ const ProductList = () => {
 
     if (query) {
         filteredProducts = products.filter((product) => product.title.toLowerCase().includes(query.toLowerCase()));
-        if  (filteredProducts.length === 0) {
+
+        if (filteredProducts.length === 0) {
             return <p>Geen producten gevonden</p>;
         }
     }
+
 
     return (
         <div className="product-container">
             <div className="product-list">
                 {filteredProducts.map((product) => (
-                    <ProductCard key={product.id} product={product} />
+                    <ProductCard key={product.id} product={product} callback={useHandleSelectProduct} />
                 ))}
             </div>
         </div>
