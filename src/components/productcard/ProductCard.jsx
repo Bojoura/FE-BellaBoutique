c109099b6c15ecdfec0cd8e8e25edb743997d950
Bojoura <bo.efendi@gmail.com>
2025-01-02 13:40:22 +0100

    Implemented the product(prop) types into productcard

--- src/components/productcard/ProductCard.jsx
+++ src/components/productcard/ProductCard.jsx
@@ -1,25 +1,21 @@
 import "./ProductCard.css";
 import PropTypes from "prop-types";
-import AccessibleImage from "../AccessibleImage.jsx";
+import AccessibleImage from "../accessible-image/AccessibleImage.jsx";
+import Product from "../../Types.js";
 
 const ProductCard = ({product, callback}) => {
     const { id, images, title, price } = product;
-    return (
-        <div className="product-item" onClick={() => callback(id)}>
-                <AccessibleImage src={images[0]} alt={title} />
-                <h3>{title}</h3>
-                <p>Prijs: € {price}</p>
-        </div>
-    );
+return (
+    <div className="product-item" onClick={() => callback(id)}>
+        <AccessibleImage src={images[0]} alt={title} />
+        <h3>{title}</h3>
+        <p>Prijs: € {price}</p>
+    </div>
+);
 };
 
 ProductCard.propTypes = {
-    product: PropTypes.shape({
-        id: PropTypes.number.isRequired,
-        images: PropTypes.arrayOf(PropTypes.string).isRequired,
-        title: PropTypes.string.isRequired,
-        price: PropTypes.number.isRequired,
-    }).isRequired,
+    product: Product,
     callback: PropTypes.func.isRequired,
 };
 
