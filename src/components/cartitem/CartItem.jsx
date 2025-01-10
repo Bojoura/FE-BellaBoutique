a65d11f9b9bfe2fec592332033b498787e63c373
Bojoura <bo.efendi@gmail.com>
2025-01-10 09:44:36 +0100

    Fixed the code (handlequantity to updatequantity)

--- src/components/cartitem/CartItem.jsx
+++ src/components/cartitem/CartItem.jsx
@@ -1,40 +1,45 @@
 import AccessibleImage from "../accessible-image/AccessibleImage.jsx";
 import PropTypes from "prop-types";
-import Product from "../../Types.js";
+import { ProductType } from "../../Types.js";
+import Button from '/src/components/Buttons/Button.jsx';
 
-const CartItem = ({item, handleQuantityChange, handleRemoveItem, label}) => {
-
+const CartItem = ({item, updateQuantity, handleRemoveItem}) => {
     return (
         <div className="cart-item" key={item.id}>
             <div className="cart-item-image">
-                <AccessibleImage src={item.images[0]} alt={item.title} />
+                <AccessibleImage 
+                    src={item.photo || item.images?.[0]} 
+                    alt={item.productName || item.title} 
+                />
             </div>
             <div className="cart-item-info">
-                <h3>{item.title}</h3>
+                <h3>{item.productName || item.title}</h3>
                 <p>Prijs: €{item.price}</p>
                 <div className="quantity-control">
-                    <label htmlFor={`quantity-${item.id}`}>{label}</label>
+                    <label htmlFor={`quantity-${item.id}`}>Aantal:</label>
                     <input
                         id={`quantity-${item.id}`}
                         type="number"
                         value={item.quantity}
                         min="1"
-                        onChange={(e) => handleQuantityChange(item.id, e.target.value)}
+                        max={item.stock}
+                        onChange={(e) => updateQuantity(item.id, parseInt(e.target.value))}
                     />
                 </div>
-                <button className="remove-item-btn" onClick={() => handleRemoveItem(item.id)}>
+                <Button
+                    className="remove-item-btn"
+                    onClick={() => handleRemoveItem(item.id)}>
                     Verwijderen
-                </button>
+                </Button>
             </div>
         </div>
     );
 };
 
 CartItem.propTypes = {
-    item: Product,
-    handleQuantityChange: PropTypes.func.isRequired,
-    handleRemoveItem: PropTypes.func.isRequired,
-    label: PropTypes.string.isRequired,
+    item: ProductType.isRequired,
+    updateQuantity: PropTypes.func.isRequired,
+    handleRemoveItem: PropTypes.func.isRequired
 };
 
 export default CartItem;
