5d5b970a05f016f5b94b879fe12ec34dbdf26905
Bojoura <bo.efendi@gmail.com>
2025-01-10 14:13:25 +0100

    added

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'bundle.js',
+        assetFileNames: ({name}) => {
+          if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')) {
+            return 'images/[name][extname]'
+          }
+          return '[name][extname]'
+        }
+      }
+    }
+  }
 })
